a6f65a8c568936028d2ed5131dfe16858cbeb360
Rodrigo Peplau <rpeplau@nishtechinc.com>
2020-09-23 07:25:15 -0300

    Assembly Info & release packages (untested)

--- src/Foundation/Azure/code/Properties/AssemblyInfo.cs
+++ src/Foundation/Azure/code/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IoTHub.Foundation.Azure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("by Rodrigo Peplau")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IoTHub.Foundation.Azure")]
+[assembly: AssemblyCompany("Nish Tech, Inc.")]
+[assembly: AssemblyProduct("Sitecore IoT Hub")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
